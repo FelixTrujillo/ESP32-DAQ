bc9fcc2ac7c88430c2f2387d629b024ff774381a
Felix Adrian Trujillo Perdomo <115059789+FelixTrujillo@users.noreply.github.com>
2024-06-20 19:55:03 -0500

    Sensor-Calibration
    Calibration of the current, temperature sensors

--- Data Acquisition System - Bicineta/CodeESP32/CodeESP32.ino
+++ Data Acquisition System - Bicineta/CodeESP32/CodeESP32.ino
@@ -36,9 +36,10 @@ double altGPS;
 int hourGPS;
 int minGPS;
 int yearGPS;
-double a = -2.2389e-3;
-double b = 5.0061e-4;
-double c = -5.9854e-7;
+double a = -0.010383;
+double b = 0.0022887;
+double c = -0.00012917;
+double d = 2.4609e-06;
 
 volatile int PPSCounter = 0;
 unsigned int pulsos = 0;
@@ -46,8 +47,8 @@ unsigned Time = 0;
 unsigned int RPM = 0; // Almacenador de valores de encoder
 int PPR = 1;
 
-double mVperAmp = 0.100;
-int muestras = 1500;
+double mVperAmp = 0.1;
+int muestras = 10000;
 
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(2);
@@ -106,13 +107,13 @@ void loop() {
   // lectura de datos de tension y corriente
   temperatura = getTemp(muestras);
   voltaje = getVoltage(muestras);
-  if (voltaje == 0) {
-    corriente = 0;
-  }
-  else {
-    corriente = getCurrent(muestras);
+  //if (voltaje == 0) {
+  //  corriente = 0;
+  //}
+  //else {
+  corriente = getCurrent(muestras);
     //corriente1 = getCurrent1(muestras);
-  }
+  //}
 
 
 
@@ -133,7 +134,6 @@ void loop() {
       }
     }
   }
-
   // Creacion String datos para la generacion del formato .CSV
         if (archivo) {
           datos = String(String(RPM) + "," + String(voltaje) + "," + String(corriente) + "," + String(corriente1) + "," + String(temperatura) +
@@ -204,15 +204,16 @@ double getCurrent(int numMuestras)
 {
   double vSensor = 0;
   double iSensor = 0;
-
-  for (int i = 0; i < numMuestras; i++) {
-    vSensor += analogRead(pinI);
-  }
-  double Vmed = ((vSensor / numMuestras) * (3.3 / 4096.0));
   double error = 0;
-  iSensor = ((Vmed - 1.494) / mVperAmp);
-  error = (0.8338 * iSensor + 0.231);
+  for (int i = 0; i < numMuestras; i++) {
+    vSensor = analogRead(pinI) * (3.3 / 4096.0);
+    iSensor += (vSensor-1.5)/mVperAmp;
+  }
+  
+  iSensor = (iSensor/numMuestras);
+  error = 0.73254*iSensor-64.196e-3;
   iSensor = iSensor + error;
+  
   return iSensor;
 }
 
@@ -224,7 +225,7 @@ double getVoltage(int numMuestras1)
     vSensor1 = analogRead(pinV) * 2 / 100.0;
     volt += vSensor1;
   }
-  volt = (volt / numMuestras1) - 0.47;
+  volt = (volt / numMuestras1);
   if (volt < 0) {
     volt = 0;
   }
@@ -239,6 +240,7 @@ double getVoltage(int numMuestras1)
 //    iSensor1 = analogRead(pin_I);
 //    current += iSensor1;
 //  }
+
 //  current = (current / numMuestras1);
 //
 //  return current;
@@ -260,6 +262,6 @@ double getTemp(int numMuestras1)
 
   V = V / numMuestras1;
   RT = R2 / ((Vin / V) - 1);
-  T = (1 / (a + b * log(RT) + c * pow(log(RT), 3))) - 273.15;
+  T = (1 / (a + b * log(RT) + c * pow(log(RT), 2) + d * pow(log(RT), 3))) - 273.15;
   return T;
 }
