ab1c102b7d27d62351a2228eb3522e08e32d5fe4
Felix Adrian Trujillo Perdomo <115059789+FelixTrujillo@users.noreply.github.com>
2024-07-22 09:52:24 -0500

    .gitignore update

---
+++ DAQ-Interface/Assets/SimpleMqtt/Demo/SampleScene.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rocworks.Mqtt
+{
+    public class SampleScene : MonoBehaviour
+    {
+        public MqttClient MqttClient;
+        public GameObject ObjectToHandle;
+        public Toggle IsConnected;
+        private InputField HostInput;
+        private InputField PortInput;
+        private InputField UsernameInput;
+        private InputField PasswordInput;
+
+        private float _floatMessage = 0;
+        private string _boolMessage = "";
+        private string _stringMessage = "";
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            HostInput.onEndEdit.AddListener(OnInputTextHost);
+            PortInput.onEndEdit.AddListener(OnInputTextPort);
+            UsernameInput.onEndEdit.AddListener(OnInputTextUsername);
+            PasswordInput.onEndEdit.AddListener(OnInputTextPassword);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+            if(_stringMessage == "ON" || _boolMessage == "True" || _floatMessage >= 0)
+            {
+                this.ObjectToHandle.SetActive(true);
+            }
+            else if(_stringMessage == "OFF" || _boolMessage == "False")
+            {
+                this.ObjectToHandle.SetActive(false);
+            }
+                
+            this.IsConnected.isOn = this.MqttClient.Connection.GetConnectState();
+        }
+
+
+        // Configurations
+        public void OnInputTextHost(string value)
+        {
+            MqttClient.Host = value;
+            
+        }
+
+        public void OnInputTextPort(string value)
+        {
+            if (int.TryParse(value, out int port))
+            {
+                MqttClient.Port = port;
+            }
+            else
+            {
+                Debug.LogError("Invalid port number");
+            }
+        }
+
+        public void OnInputTextUsername(string value)
+        {
+            MqttClient.Username = value;
+        }
+
+        public void OnInputTextPassword(string value)
+        {
+            MqttClient.Password = value;
+        }
+
+        public void OnToggleButtonWebSocket(bool value)
+        {
+            MqttClient.Websocket = value;
+        }
+
+        public void OnInputTextWebsocketPath(string value)
+        {
+            MqttClient.WebsocketPath = value;
+        }
+
+        public void OnToggleButtonUseTLS(bool value)
+        {
+            MqttClient.UseTLS = value;
+        }
+
+        public void OnToggleButtonAllowUntrusted(bool value)
+        {
+            MqttClient.AllowUntrusted = value;
+        }
+
+        public void OnToggleButtonCleanSession(bool value)
+        {
+            MqttClient.CleanSession = value;
+        }
+
+
+
+
+        // Messages
+
+        public void OnToggleButtonChanged(bool value)
+        {
+            Debug.Log("OnToggleButtonChanged");
+            this.MqttClient.Connection.SetConnectFlag(value);
+        }
+
+        public void OnSliderTurnLed(float value)
+        {
+            MqttClient.Connection.Publish("Rexroth/float/message", value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public void OnToggleTurnLed(bool value)
+        {
+            MqttClient.Connection.Publish("Rexroth/bool/message", value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public void OnInputTextTurnLed(string value)
+        {
+            MqttClient.Connection.Publish("Rexroth/string/message", value);
+        }
+
+        
+
+        public void SetDebug(bool value)
+        {
+            MqttClient.DebugMessages = value;
+        }
+
+        public void OnMessageArrived(MqttMessage m)
+        {
+            switch (m.GetTopic())
+            {
+                case "Rexroth/float/message":
+                    _floatMessage = float.Parse(m.GetString(), CultureInfo.InvariantCulture);
+                    break;
+                case "Rexroth/bool/message":
+                    _boolMessage = m.GetString();
+                    break;
+                case "Rexroth/string/message":
+                    _stringMessage = m.GetString();
+                    break;
+                
+            }
+        }
+    }
+}
