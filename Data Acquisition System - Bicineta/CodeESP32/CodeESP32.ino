6a43b97b63fa261322a2d1b6ba6a547a5e0c9dd6
Felix Adrian Trujillo Perdomo <115059789+FelixTrujillo@users.noreply.github.com>
2024-07-29 13:57:51 -0500

    DAQ-V2-KICAD-modification

--- Data Acquisition System - Bicineta/CodeESP32/CodeESP32.ino
+++ Data Acquisition System - Bicineta/CodeESP32/CodeESP32.ino
@@ -20,10 +20,13 @@ double pinT = 34; // Pin medir temperatura T-D500
 
 int pinCS = 5; // Pin modulo memoria
 
-// Inicializacion variables de corriente, tension y temperatura
+int pinACC = 25; // Pin Acelerador
+
+// Inicializacion variables de corriente, tension, temperatura y aceleracion
 double corriente = 0;
 double temperatura = 0;
 double voltaje = 0;
+double accelerator = 0;
 
 // Inicializacion vaiables del GPS
 double latGPS;
@@ -47,8 +50,8 @@ unsigned int pulsos = 0;     // Cantidad de pulsos en 1 segundo
 unsigned Time = 0;           // Inicialización variable de tiempo
 float RPM = 0;        // Inicializacion variable de velocidad actual
 unsigned int RPM_ANT = 0;    // Inicializacion variable de velocidad anterior
-int PPR = 1;                // Numero de pulsos que detecta el sensor por vuelta
-double debounceDelay = 250;
+int PPR = 2;                // Numero de pulsos que detecta el sensor por vuelta
+double debounceDelay = 120;
 double debounceTime = 0;
 double lastDebounceTime = 0;
 
@@ -56,7 +59,7 @@ double lastDebounceTime = 0;
 double mVperAmp = 0.1;       // Sensibilidad sensor de corriente ACS712
 
 // Numero de muestras para la medicion de datos
-int muestras = 10000;
+int muestras = 1000;
 
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(2);
@@ -69,7 +72,7 @@ int getRPM();
 double getCurrent(int numMuestras);
 double getVoltage(int numMuestras1);
 double getTemp(int numMuestras1);
-
+double getSignalAcceleration(int numMuestras1);
 
 void setup() {
 
@@ -99,7 +102,7 @@ void setup() {
     return;
   }
 
-  archivo.println("Velocidad [RPM],Voltaje[V],Corriente[A],Temperatura[°C],Latitud,Longitud,Velocidad[GPS: km/h],Satelites, Altitud, Hora, Minutos, Year");
+  archivo.println("Velocidad [RPM],Aceleracion [V_Acc], Voltaje[V],Corriente[A],Temperatura[°C],Latitud,Longitud,Velocidad[GPS: km/h],Satelites, Altitud, Hora, Minutos, Year");
   archivo.close();
 }
 
@@ -109,12 +112,13 @@ void loop() {
 
   // Lectura de velocidad con el encoder
   RPM = getRPM();
-
+  accelerator = getSignalAcceleration(muestras); 
   // Lectura de temperatura
   temperatura = getTemp(muestras);
 
   // lectura de datos de tension y corriente
   voltaje = getVoltage(muestras);
+
   if (voltaje == 0 || corriente < 0) {
     corriente = 0;
   }
@@ -138,11 +142,12 @@ void loop() {
         yearGPS = gps.date.year();
 
         
-        
-  }}};
+      };
+    };
+  };
   // Creacion String datos para la generacion del formato .CSV
   if (archivo) {
-          datos = String(String(RPM) + "," + String(voltaje, 8) + "," + String(corriente, 8) + "," + String(temperatura, 8) +
+          datos = String(String(RPM) +","+ String(accelerator) + "," + String(voltaje, 8) + "," + String(corriente, 8) + "," + String(temperatura, 8) +
                          "," + String(latGPS, 15) + "," + String(longGPS, 15) + "," + String(velGPS, 8) + "," + String(numSat) + "," + String(altGPS, 8) + "," + String(hourGPS) + "," + String(minGPS) +
                          "," + String(yearGPS));
 
@@ -152,44 +157,47 @@ void loop() {
           archivo.close();
         };
 
-        // Serial.print("Velocidad [RPM]: ");
-        // Serial.print(RPM);
-        // Serial.print(",");
-        // Serial.print("Voltaje [V]: ");
-        // Serial.print(voltaje);
-        // Serial.print(",");
-        // Serial.print("Corriente [A]: ");
-        // Serial.print(corriente);
-        // Serial.print(",");
-        // Serial.print("Temperatura [°C]: ");
-        // Serial.print(temperatura);
-        // Serial.print(",");
-        // // Lectura de datos GPS
-        // Serial.print("Latitud: ");
-        // Serial.print(latGPS);
-        // Serial.print(",");
-        // Serial.print("Longitud: ");
-        // Serial.print(longGPS);
-        // Serial.print(",");
-        // Serial.print("Velocidad [km/h]: ");
-        // Serial.print(velGPS);
-        // Serial.print(",");
-        // Serial.print("Satelites: ");
-        // Serial.print(numSat);
-        // Serial.print(",");
-        // Serial.print("Altitud: ");
-        // Serial.print(altGPS);
-        // Serial.print(",");
-        // Serial.print("Hora: ");
-        // Serial.print(hourGPS);
-        // Serial.print(",");
-        // Serial.print("Minutos: ");
-        // Serial.print(minGPS);
-        // Serial.print(",");
-        // Serial.print("Año: ");
-        // Serial.println(yearGPS);
+        Serial.print("Velocidad [RPM]: ");
+        Serial.print(RPM);
+        Serial.print(",");
+        Serial.print("Aceleracion [V_Acc]: ");
+        Serial.print(accelerator);
+        Serial.print(",");
+        Serial.print("Voltaje [V]: ");
+        Serial.print(voltaje);
+        Serial.print(",");
+        Serial.print("Corriente [A]: ");
+        Serial.print(corriente);
+        Serial.print(",");
+        Serial.print("Temperatura [°C]: ");
+        Serial.print(temperatura);
+        Serial.print(",");
+        // Lectura de datos GPS
+        Serial.print("Latitud: ");
+        Serial.print(latGPS);
+        Serial.print(",");
+        Serial.print("Longitud: ");
+        Serial.print(longGPS);
+        Serial.print(",");
+        Serial.print("Velocidad [km/h]: ");
+        Serial.print(velGPS);
+        Serial.print(",");
+        Serial.print("Satelites: ");
+        Serial.print(numSat);
+        Serial.print(",");
+        Serial.print("Altitud: ");
+        Serial.print(altGPS);
+        Serial.print(",");
+        Serial.print("Hora: ");
+        Serial.print(hourGPS);
+        Serial.print(",");
+        Serial.print("Minutos: ");
+        Serial.print(minGPS);
+        Serial.print(",");
+        Serial.print("Año: ");
+        Serial.println(yearGPS);
         
-        delay(100);
+        delay(1000);
       
 }
 
@@ -214,26 +222,26 @@ int getRPM(){
 
       pulsos = PPSCounter;
       RPM_ANT = RPM;
-      RPM = (60 * pulsos / PPR);
+      RPM = (60 * pulsos / (PPR));
       PPSCounter = 0;
       pulsos = 0;
       Time = millis();
 
   }
 
-  if(RPM <= 350){
-    debounceDelay = 300;
+  if(RPM <= 450){
+    debounceDelay = 93;
   }
-  else if(RPM > 350 && RPM <= 750){
-      debounceDelay = 120;
+  else if(RPM > 450 && RPM <= 750){
+      debounceDelay = 53;
   }
   else if(RPM > 750 && RPM <= 1000){
-    debounceDelay = 70;
+    debounceDelay = 33;
   }
   else if(RPM > 1000){
-    debounceDelay = 32;
+    debounceDelay = 5;
   }
-
+  Serial.println(debounceDelay);
   return RPM;
 
 }
@@ -275,20 +283,32 @@ double getVoltage(int numMuestras1){
 }
 
 double getTemp(int numMuestras1){
-  double RT = 0;
-  double R2 = 150e3;
-  double Vin = 3.3;
+  //double RT = 0;
+  //double R2 = 150e3;
+  //double Vin = 3.3;
   double Vout = 0;
   double V = 0;
   double T = 0;
-
-  for (int k = 0; k < numMuestras1; k++) {
+  int numMuestras2 = numMuestras1 + 5000;
+  for (int k = 0; k < numMuestras2; k++) {
     Vout = analogRead(pinT) * 3.3 / 4095;
     V += Vout;
   }
 
-  V = V / numMuestras1;
-  RT = R2 / ((Vin / V) - 1);
-  T = (1 / (a + b * log(RT) + c * pow(log(RT), 2) + d * pow(log(RT), 3))) - 273.15;
+  V = V / numMuestras2;
+  //RT = R2 / ((Vin / V) - 1);
+  //T = (1 / (a + b * log(RT) + c * pow(log(RT), 2) + d * pow(log(RT), 3))) - 273.15;
+  T = 153*exp(-0.87*V);
   return T;
+}
+
+double getSignalAcceleration(int numMuestras1){
+  double SignalAcc = 0 ;
+  double VAcc = 0;
+  for(int i = 0; i < numMuestras1; i++ ){
+    VAcc = analogRead(pinACC)*3.3/4095;
+    SignalAcc += VAcc;
+  }
+  SignalAcc = SignalAcc / numMuestras1;
+  return SignalAcc;
 }
